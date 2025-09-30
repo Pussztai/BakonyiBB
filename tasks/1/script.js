3b3faf8fb6d36cf42f23d3963706da1dc7c2f141
PuszAtiCkik <pusztai.attila@gmail.hu>
2025-09-30 22:24:19 +0200

    le kell hozni

--- tasks/1/script.js
+++ tasks/1/script.js
@@ -1,16 +1,14 @@
 
 
-
-document.addEventListener('DOMContentLoaded', function() {
-    const toggleButtons = [
+const toggleButtons = [
         { btnId: 'toggleBtnAti', targetId: 'AtiP' },
         { btnId: 'toggleBtnDave', targetId: 'DaveP' },
         { btnId: 'toggleBtnDomi', targetId: 'DomiP' }
     ];
 
-    toggleButtons.forEach(({ btnId, targetId }) => {
-        const button = document.getElementById(btnId);
-        const target = document.getElementById(targetId);
+toggleButtons.forEach(({ btnId, targetId }) => {
+    const button = document.getElementById(btnId);
+    const target = document.getElementById(targetId);
         
         if (button && target) {
             button.addEventListener('click', function() {
@@ -42,7 +40,6 @@ setInterval(updateClock, 1000);
 
 updateClock();
 
-}); 
 
 
 const gomb = document.querySelector(".extraKepek");
@@ -51,15 +48,15 @@ const descript = document.getElementById("diaPictureDescription")
 
 const kepek = [
     "images/erasmus.png",
-    "https://via.placeholder.com/200x150?text=Kép+2",
-    "https://via.placeholder.com/200x150?text=Kép+3"
+    "images/technikumi_rangsor.png",
+    "images/cegléd_varos.jpg"
   ];
 
 
 const leirasok = [
-    "kurva",
-    "nurva",
-    "durva"
+    "Erasmus Program 2024",
+    "Országos Technikumok ranglistája (13.Helyezés)",
+    "Cegléd"
 ]
 
 let index = -1;
@@ -79,10 +76,10 @@ gomb.addEventListener("click", () => {
       gomb.textContent = "Következő kép";
       descript.textContent = leirasok[index];
     } else {
-      // ha elérte a véget
-      kepHely.innerHTML = ""; // eltünteti a képet
-      descript.textContent = "";       // szöveg eltüntetése
+      // ha elerte a veget
+      kepHely.innerHTML = ""; 
+      descript.textContent = "";       
       gomb.textContent = "Nyomj ide Extra képekért";
-      index = -1; // visszaállítjuk, mintha újrakezdődne
+      index = -1;
     }
   });
